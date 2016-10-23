22e1becacb84b10b88bc07fc07663ee11ec96624
Santiago Saavedra <santiagosaavedra@gmail.com>
2016-10-23 17:36:41 +0200

    Change to pub/sub and add stub twitter client

--- right-here-api/index.js
+++ right-here-api/index.js
@@ -3,28 +3,32 @@ var zmq = require('zmq');
 
 var app = express();
 
-var sender = zmq.socket('push');
+var sender = zmq.socket('pub');
 sender.bindSync('tcp://127.0.0.1:3000');
 
-var receiver = zmq.socket('pull');
+var receiver = zmq.socket('sub');
 receiver.bindSync('tcp://127.0.0.1:3333');
+receiver.subscribe('')
 var resultsBuffer = {};
 
 receiver.on('message', function(msg) {
+  console.log("I got a message", msg, "\n\n")
+  if(msg == 'response') return
   msg = JSON.parse(msg);
   if (!resultsBuffer[msg.replyId]) {
-      resultsBuffer[msg.replyId] = []
+    resultsBuffer[msg.replyId] = []
   }
-  resultsBuffer[msg.replyId].push(msg.data);//Componer resultados
+  resultsBuffer[msg.replyId] = resultsBuffer[msg.replyId].concat(msg.data);//Componer resultados
+  console.log("I have results..", resultsBuffer[msg.replyId])
 });
 
 app.get('/geotimeline', function(req, res) {
-  //res.json({clave:'Hola mundo'});
   var msgId = 1;
-  sender.send(JSON.stringify({replyId: msgId, data: {lat: 43.02345, lon: -7.23456}}));
+  sender.send(['request', JSON.stringify({replyId: msgId, data: {lat: 43.02345, lon: -7.23456}})]);
   setTimeout(function() {
-    res.send(resultsBuffer[msgId]);
-  },5000);
+    res.send(JSON.stringify(resultsBuffer[msgId]));
+    resultsBuffer[msgId] = undefined;
+  }, 1000);
 });
 
 app.listen(8000, function() {
---
+++ twitter-us/main.py
@@ -0,0 +1,65 @@
+
+import codecs, json, os, time
+import zmq
+
+
+context = zmq.Context()
+
+def setup_queues():
+    receiver = context.socket(zmq.SUB)
+    sender = context.socket(zmq.PUB)
+
+    receiver.connect(os.environ.get('ZMQ_PULL_QUEUE', 'tcp://127.0.0.1:3000'))
+    sender.connect(os.environ.get('ZMQ_PUSH_QUEUE', 'tcp://127.0.0.1:3333'))
+    receiver.subscribe('request')
+
+    return receiver, sender
+
+
+def ask_twitter(lat, lon):
+    tweets = []
+    tweets.append({
+        'data': {
+            'id': 23456789876543,
+            'username': '@ssice',
+            'text': 'Hackatinho happened here',
+            'lat': lat,
+            'lon': lon,
+        },
+        'type': 'twitter',
+        'version': 1,
+    })
+
+    return tweets
+
+
+def main():
+    receiver, sender = setup_queues()
+
+    should_exit = False
+
+    while not should_exit:
+        msg = receiver.recv().decode('utf-8')
+        msg = receiver.recv().decode('utf-8')
+        print("Got message: {0}".format(msg))
+        msg = json.loads(msg)
+        if 'control' in msg and msg['control'] == 'EXIT':
+            break
+        msgId = msg['replyId']
+        data = msg['data']
+        lat, lon = data['lat'], data['lon']
+
+        tweets = ask_twitter(lat, lon)
+
+        envelope = {
+            'replyId': msgId,
+            'data': tweets,
+        }
+
+        print("Sending in return: {0} / {1}".format(tweets, type(json.dumps(envelope))))
+        sender.send(b'response')
+        sender.send(json.dumps(envelope).encode())
+
+
+if __name__ == '__main__':
+    main()
