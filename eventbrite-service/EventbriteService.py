05fd15177f6e51779019ef4110fec6410f2ae6f8
Carlos Garcia <carlisgg@gmail.com>
2016-10-23 21:41:54 +0200

    Request eventbrite events from API and send results through 0MQ

--- eventbrite-service/EventbriteService.py
+++ eventbrite-service/EventbriteService.py
@@ -1,5 +1,8 @@
 import zmq
 import json
+import requests
+import os
+
 from datetime import datetime
 
 context = zmq.Context()
@@ -9,16 +12,29 @@ subscriber.connect("tcp://127.0.0.1:3000")
 publisher = context.socket(zmq.PUSH)
 publisher.connect("tcp://127.0.0.1:3333")
 
+def decorate_event(event):
+    event['type'] = 'eventbrite'
+    return event
+
 if __name__ == '__main__':
+    token = os.environ.get("EVENTBRITE_TOKEN", "Not set")
+    headers = {'Authorization': 'Bearer ' + token}
+
     message = subscriber.recv()
     message = json.loads(message)
-    print message["replyId"]
 
-    message["data"] = {
-        "tipo": "eventbrite",
-        "version": 1,
-        "fecha": 'date',
-        "lat": 1,
-        "lon": 2
+    queryparams = {
+        "location.latitude": message["data"]["lat"],
+        "location.longitude": message["data"]["lon"]
     }
-    publisher.send(json.dumps(message))
+
+    url = 'https://www.eventbriteapi.com/v3/events/search/'
+    res = requests.get(url, headers=headers, params=queryparams)
+
+    json_body = json.loads(res.text)
+    data = map(decorate_event, json_body['events'])
+
+    message["data"] = data
+    dump = json.dumps(message)
+    print dump
+    publisher.send(dump)
