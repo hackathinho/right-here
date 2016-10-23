592ca5a938236ea12fd8f0f2436b0758f9602d01
Carlos Garcia <carlisgg@gmail.com>
2016-10-23 13:56:39 +0200

    Parsing JSON and returning skeleton for data

--- eventbrite-service/EventbriteService.py
+++ eventbrite-service/EventbriteService.py
@@ -1,4 +1,6 @@
 import zmq
+import json
+from datetime import datetime
 
 context = zmq.Context()
 subscriber = context.socket(zmq.PULL)
@@ -9,5 +11,14 @@ publisher.connect("tcp://127.0.0.1:3333")
 
 if __name__ == '__main__':
     message = subscriber.recv()
-    print message
-    publisher.send(b"hola")
+    message = json.loads(message)
+    print message["replyId"]
+
+    message["data"] = {
+        "tipo": "eventbrite",
+        "version": 1,
+        "fecha": 'date',
+        "lat": 1,
+        "lon": 2
+    }
+    publisher.send(json.dumps(message))
